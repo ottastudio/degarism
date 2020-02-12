841faacd893c877d30dcfa81a8c555136a7e6943
Taufik Oktama <oktamataufik@gmail.com>
2020-02-12 23:52:02 +0700

    Improved site data api

--- src/pages/api/v1/sites/data.ts
+++ src/pages/api/v1/sites/data.ts
@@ -2,16 +2,30 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { withDB } from "../../../../lib/database";
 import { useModels } from "../../../../lib/database/useModels";
 
-const siteData = async (_req: NextApiRequest, res: NextApiResponse) => {
+const siteData = async (req: NextApiRequest, res: NextApiResponse) => {
+  const {
+    method,
+    headers: { referer }
+  } = req;
   const { Site } = await useModels();
 
-  try {
-    const response = await Site.find({});
-    const siteData = await response;
-    res.status(200).json({ success: true, siteData });
-    res.end();
-  } catch (error) {
-    res.status(400).end();
+  if (referer !== undefined) {
+    switch (method) {
+      case "GET":
+        const response = await Site.find({});
+        const siteData = await response;
+        res.status(200).json({ success: true, siteData: siteData[0] });
+        res.end();
+        break;
+      case "POST":
+        break;
+      default:
+        break;
+    }
+  } else {
+    // res.writeHead(302, { Location: "/?redirected=true" });
+    // res.end();
+    res.status(403).send("You're not allowed!");
   }
 };
 
