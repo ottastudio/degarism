047d5374a1ac12b8a539ed835aacc69305867c79
Taufik Oktama <oktamataufik@gmail.com>
2020-02-15 10:04:42 +0700

    Change to markup string

--- src/lib/database/schemas/faq_schema.ts
+++ src/lib/database/schemas/faq_schema.ts
@@ -3,8 +3,7 @@ import { Type, createSchema } from "ts-mongoose";
 export default createSchema(
   {
     topic: Type.string({ required: true }),
-    question: Type.string({ required: true }),
-    answer: Type.string({ required: true })
+    markup: Type.string({ required: true })
   },
   { timestamps: { createdAt: true } }
 );
