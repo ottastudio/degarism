0cd443f1fb0055cf22df09094104a1825bb6e7d4
Taufik Oktama <oktamataufik@gmail.com>
2020-02-15 10:08:13 +0700

    Create dashboard products

---
+++ src/components/Etc/Admin/Products/index.tsx
@@ -0,0 +1,7 @@
+export interface DashboardProductsProps {}
+
+const DashboardProducts: React.FC<DashboardProductsProps> = () => {
+  return <div>Dashboard Products</div>;
+};
+
+export default DashboardProducts;
