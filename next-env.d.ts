3faea4a4b84f9ad46142bb0f435931f00e1460b5
Taufik Oktama <oktamataufik@gmail.com>
2020-02-15 09:25:37 +0700

    Add ckeditor

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-declare module 'next-page-transitions';
+declare module "next-page-transitions";
+declare module "@ckeditor/ckeditor5-react";
+declare module "@ckeditor/ckeditor5-build-balloon";
