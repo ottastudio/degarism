1800c751ea3639c03793b457989e71a966dda08d
Taufik Oktama <oktamataufik@gmail.com>
2020-02-17 00:00:47 +0700

    Add SESSION_SECRET

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 module.exports = {
   poweredByHeader: false,
   env: {
-    MONGODB_URI: process.env.MONGODB_URI
+    MONGODB_URI: process.env.MONGODB_URI,
+    SESSION_SECRET: process.env.SESSION_SECRET
   }
 };
