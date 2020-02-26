ddf60929658c91c7453b8105882c56b64dd12581
Taufik Oktama <oktamataufik@gmail.com>
2020-02-26 17:41:05 +0700

    Specific handler and passing db model directly

--- src/lib/database/index.ts
+++ src/lib/database/index.ts
@@ -1,26 +1,34 @@
 import mongoose from "mongoose";
 import { NextApiRequest, NextApiResponse } from "next";
+import { IDatabase } from "../../../global";
 
-export const withDB = (handler: any) => async (
-  req: NextApiRequest,
-  res: NextApiResponse
-) => {
-  if (mongoose.connection.readyState) return handler(req, res);
+import { User } from "./schemas/user_schema";
+import { Faq } from "./schemas/faq_schema";
+import { Site } from "./schemas/site_schema";
+import { Category } from "./schemas/category_schema";
 
-  mongoose.Promise = global.Promise;
+export const withDB = (
+  handler: (
+    req: NextApiRequest,
+    res: NextApiResponse,
+    db: IDatabase
+  ) => Promise<void>
+) => async (req: NextApiRequest, res: NextApiResponse) => {
+  const db = { User, Faq, Site, Category };
+
+  if (mongoose.connection.readyState) return handler(req, res, db);
   return await mongoose
     .connect(process.env.MONGODB_URI as string, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
+      useCreateIndex: true
     })
     .then(responseDB => {
       console.log(`🚀 CONNECTION TO MONGODB ESTABLISHED`);
       console.log(
         `💾 ${responseDB.connections[0].host}:${responseDB.connections[0].port}/${responseDB.connections[0].name}`
       );
-      return handler(req, res);
+      return handler(req, res, db);
     })
     .catch(err => {
       const errHandler = (_req: NextApiRequest, res: NextApiResponse) => {
