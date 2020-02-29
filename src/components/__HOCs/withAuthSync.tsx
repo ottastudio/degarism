e1ba408ce5d5cc18d34f33b65743af8305fba1d5
Taufik Oktama <oktamataufik@gmail.com>
2020-02-29 12:40:27 +0700

    Improved with token checker expired

--- src/components/__HOCs/withAuthSync.tsx
+++ src/components/__HOCs/withAuthSync.tsx
@@ -1,10 +1,11 @@
 import { DocumentContext } from "next/document";
-import React, { useEffect } from "react";
+import { verify, VerifyErrors } from "jsonwebtoken";
+import { useEffect, ReactNode } from "react";
 import Router from "next/router";
 import nextCookie from "next-cookies";
 
 type WrapperProps = JSX.IntrinsicAttributes & {
-  children?: React.ReactNode;
+  children?: ReactNode;
 };
 
 export const withAuthSync = (WrappedComponent: any) => {
@@ -28,12 +29,35 @@ export const withAuthSync = (WrappedComponent: any) => {
 
   Wrapper.getInitialProps = async (ctx: DocumentContext) => {
     const { token } = nextCookie(ctx);
+
     if (!token) {
       if (typeof window === "undefined") {
         ctx.res?.writeHead(302, { Location: "/etc/account" }).end();
+        return;
       } else {
         Router.push("/etc/account");
+        return;
       }
+    } else {
+      verify(
+        token as string,
+        process.env.SESSION_SECRET as string,
+        (err: VerifyErrors) => {
+          if (err) {
+            if (typeof window === "undefined") {
+              ctx.res?.writeHead(302, { Location: "/etc/account" }).end();
+              console.log("Server token expired");
+              return;
+            } else {
+              Router.push("/etc/account");
+              console.log("Client token expired");
+              return;
+            }
+          } else {
+            return;
+          }
+        }
+      );
     }
 
     const componentProps =
