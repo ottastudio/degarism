f30e3224cfa82831b05e088d1cfff8b31b604423
Taufik Oktama <oktamataufik@gmail.com>
2020-02-29 21:52:47 +0700

    Using auth checker hoc

--- src/pages/etc/account.tsx
+++ src/pages/etc/account.tsx
@@ -1,9 +1,8 @@
 import { NextPage } from "next";
-import { DocumentContext } from "next/document";
-import { AccordionContainer } from "../../components/Utils/Accordion";
 import Head from "next/head";
-import Router from "next/router";
-import nextCookie from "next-cookies";
+
+import { AccordionContainer } from "../../components/Utils/Accordion";
+import { withAuthCheck } from "../../components/__HOCs/withAuthCheck";
 
 import LoginForm from "../../components/Etc/Account/loginForm";
 import RegisterForm from "../../components/Etc/Account/registerForm";
@@ -22,20 +21,4 @@ const Account: NextPage<{}> = () => {
   );
 };
 
-Account.getInitialProps = async (ctx: DocumentContext) => {
-  const { token } = nextCookie(ctx);
-  try {
-    if (token) {
-      if (typeof window === "undefined") {
-        ctx.res?.writeHead(302, { Location: "/etc/dashboard" });
-      } else {
-        Router.push("/etc/dashboard");
-      }
-    }
-    return { token };
-  } catch (error) {
-    throw error;
-  }
-};
-
-export default Account;
+export default withAuthCheck(Account);
